5bc627713d0e42389e67428e3c5b801feddc42aa
Samandar Turgunboyev <turgunboyevsamandar4@gmail.com>
2025-11-26 09:26:06 +0500

    gtm added

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -74,6 +74,23 @@ export default async function RootLayout({ children, params }: Props) {
       <body
         className={`${sfPro.className} antialiased min-h-screen flex flex-col relative`}
       >
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TXPZGNKH');
+          `}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TXPZGNKH"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <NextIntlClientProvider locale={locale}>
           <ThemeProvider
             attribute={'class'}
