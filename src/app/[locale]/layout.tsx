1b00fa3307ee628758a75beb4329126989e7cb6d
Ibrohim <ibroxim.rashidov9199@gmail.com>
2026-05-15 16:20:41 +0500

    Add facebook & meta scripts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -78,6 +78,7 @@ export default async function RootLayout({ children, params }: Props) {
       <body
         className={`${monserrat.className} antialiased min-h-screen flex flex-col relative`}
       >
+        {/* Google metrika Code START */}
         <Script id="gtm-head" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -85,16 +86,59 @@ export default async function RootLayout({ children, params }: Props) {
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-TXPZGNKH');
-          `}
+            `}
         </Script>
+
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TXPZGNKH"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
+            ></iframe>
         </noscript>
+            {/* Google metrika Code END */}
+
+
+        {/* Meta Pixel Code START */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1994237517889899');
+        fbq('track', 'PageView');`}
+        </Script>
+
+        <noscript><img height="1" width="1" style={{display: 'none'}} alt=""
+        src="https://www.facebook.com/tr?id=1994237517889899&ev=PageView&noscript=1"
+        /></noscript>
+        {/* Meta Pixel Code  END*/}
+
+
+{/* Yandex Metrika */}
+<Script id="yandex-metrika" strategy="afterInteractive">
+  {` (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109235082', 'ym');
+        
+        ym(109235082, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        `}
+</Script>
+<noscript><div><img src="https://mc.yandex.ru/watch/109235082" style={{position: 'absolute', left: '-9999px'}} alt="" /></div></noscript>
+
+{/* Yandex Metrika END */}
+
+
+       
+        
         <NextIntlClientProvider locale={locale}>
           <ThemeProvider
             attribute={'class'}
